e7e8cb2cbbb2aab3ff6663b93e6167a7eb48710d
mizhuhara <110947967+mizhuhara@users.noreply.github.com>
2023-11-15 07:34:50 +0700

    Update detail.php

--- app/views/barang/detail.php
+++ app/views/barang/detail.php
@@ -5,7 +5,7 @@
             <h5 class="card-title"><?= $data['barang']['namaBarang'];?></h5>
             <h6 class="card-subtitle mb-2 text-muted"><?= $data['barang']['merek'];?></h6>
             <p class="card-text"><?= $data['barang']['stock'];?></p>
-            <img class="card-text" src="<?= BASEURL ?>/public/img/gambar/<?= $data['barang']['img'];?>" alt="">   
+            <img class="card-text" src="<?= BASEURL ?>/img/gambar/<?= $data['barang']['img'];?>" alt="">   
             <a href="<?= BASEURL;?>/barang" class="card-link">Kembali</a>
         </div>
     </div>
