fffd09d561f4c829f82e04608726325a10d626de
Dwipaaa <147568071+Dwipaaa@users.noreply.github.com>
2023-11-15 09:23:22 +0800

    Create index.php

---
+++ app/views/produk/index.php
@@ -0,0 +1,472 @@
+<section>
+  <div class="container mt-4 mx-3">
+    <form class="form-inline my-2 my-lg-0">
+      <br>
+      <br>
+      <div class="right d-flex">
+        <input class="search form-control mr-sm-2" type="search" placeholder="" aria-label="Search">
+        <div class="">
+          <select class="from-brand form-select ms-5" aria-label="Default select example">
+            <option selected>Xiaomi</option>
+            <option value="1">Oppo</option>
+            <option value="2">Samsung</option>
+            <option value="3">Vivo</option>
+            <option value="4">Iphone</option>
+          </select>
+        </div>
+        <div class="left ms-5">
+          <select class="minmax form-select ms-5" aria-label="Default select example">
+            <option selected>Harga Tertinggi</option>
+            <option value="1">Harga Terendah</option>
+            <option value="2">Terbaru</option>
+          </select>
+        </div>
+      </div>
+    </form>
+  </div>
+</section>
+<br><br><br>
+<hr class="line">
+<section class="container-fluid">
+  <div class="main-hero row mt-5 px-4">
+    <div class="card-wrapper col-2 mt-2">
+      <div class="card-1 text-center">
+        <br>
+        <p class="brand">Redmi Note 12</p>
+        <p class="harga">2.300.000</p>
+        <img class="img-fluid" src="<?=BASEURL; ?>\img\redmi note 12.svg" alt="">
+        <br><br>
+        <button class="button px-2 py-1">Beli Sekarang</button>
+        <br><br>
+        <p class="spek">6+128 GB</p>
+      </div>
+    </div>>
+
+    <div class="card-wrapper col-2 mt-2">
+      <div class="card-1 text-center">
+        <br>
+        <p class="brand">Vivo V21</p>
+        <p class="harga">3.800.000</p>
+        <img class="img-fluid" src="/image/vivo v21.svg" alt="">
+        <br><br>
+        <button class="button px-2 py-1">Beli Sekarang</button>
+        <br><br>
+        <p class="spek">8+128 GB</p>
+      </div>
+    </div>
+
+    <div class="card-wrapper col-2 mt-2">
+      <div class="card-1 text-center">
+        <br>
+        <p class="brand">Samsung s23</p>
+        <p class="harga">8.999.000</p>
+        <img class="img-fluid" src="/image/samsung s23.svg" alt="">
+        <br><br>
+        <button class="button px-2 py-1">Beli Sekarang</button>
+        <br><br>
+        <p class="spek">8+128 GB</p>
+      </div>
+    </div>
+
+    <div class="card-wrapper col-2 mt-2">
+      <div class="card-1 text-center">
+        <br>
+        <p class="brand">Oppo Reno 8T</p>
+        <p class="harga">4.799.000</p>
+        <img class="img-fluid" src="/image/oppo reno 8t.svg" alt="">
+        <br><br>
+        <button class="button px-2 py-1">Beli Sekarang</button>
+        <br><br>
+        <p class="spek">8+256 GB</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="main-hero row mt-5 px-4">
+    <div class="card-wrapper col-2 mt-2">
+      <div class="card-1 text-center">
+        <br>
+        <p class="brand">Redmi 12</p>
+        <p class="harga">1.999.000</p>
+        <img class="img-fluid" src="/image/redmi 12.svg" alt="">
+        <br><br>
+        <button class="button px-2 py-1">Beli Sekarang</button>
+        <br><br>
+        <p class="spek">8+128 GB</p>
+      </div>
+    </div>
+
+    <div class="card-wrapper col-2 mt-2">
+      <div class="card-1 text-center">
+        <br>
+        <p class="brand">Redmi Note 12 Pro</p>
+        <p class="harga">3.699.000</p>
+        <img class="img-fluid" src="/image/redmi note 12 pro.svg" alt="">
+        <br><br>
+        <button class="button px-2 py-1">Beli Sekarang</button>
+        <br><br>
+        <p class="spek">8+256 GB</p>
+      </div>
+    </div>
+
+    <div class="card-wrapper col-2 mt-2">
+      <div class="card-1 text-center">
+        <br>
+        <p class="brand">Poco X5 5g</p>
+        <p class="harga">3.300.000</p>
+        <img class="img-fluid" src="/image/Poco x5.svg" alt="">
+        <br><br>
+        <button class="button px-2 py-1">Beli Sekarang</button>
+        <br><br>
+        <p class="spek">6+128 GB</p>
+      </div>
+    </div>
+
+    <div class="card-wrapper col-2 mt-2">
+      <div class="card-1 text-center">
+        <br>
+        <p class="brand">Vivo T1 Pro 5g</p>
+        <p class="harga">4.999.000</p>
+        <img class="img-fluid" src="/image/Vivo T1 Pro 5G.svg" alt="">
+        <br><br>
+        <button class="button px-2 py-1">Beli Sekarang</button>
+        <br><br>
+        <p class="spek">8+128 GB</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="main-hero row mt-5 px-4">
+    <div class="card-wrapper col-2 mt-2">
+      <div class="card-1 text-center">
+        <br>
+        <p class="brand">Redmi Note 12</p>
+        <p class="harga">2.300.000</p>
+        <img class="img-fluid" src="/image/redmi note 12.svg" alt="">
+        <br><br>
+        <button class="button px-2 py-1">Beli Sekarang</button>
+        <br><br>
+        <p class="spek">6+128 GB</p>
+      </div>
+    </div>
+
+    <div class="card-wrapper col-2 mt-2">
+      <div class="card-1 text-center">
+        <br>
+        <p class="brand">Vivo V21</p>
+        <p class="harga">3.800.000</p>
+        <img class="img-fluid" src="/image/vivo v21.svg" alt="">
+        <br><br>
+        <button class="button px-2 py-1">Beli Sekarang</button>
+        <br><br>
+        <p class="spek">8+128 GB</p>
+      </div>
+    </div>
+
+    <div class="card-wrapper col-2 mt-2">
+      <div class="card-1 text-center">
+        <br>
+        <p class="brand">Samsung s23</p>
+        <p class="harga">8.999.000</p>
+        <img class="img-fluid" src="/image/samsung s23.svg" alt="">
+        <br><br>
+        <button class="button px-2 py-1">Beli Sekarang</button>
+        <br><br>
+        <p class="spek">8+128 GB</p>
+      </div>
+    </div>
+
+    <div class="card-wrapper col-2 mt-2">
+      <div class="card-1 text-center">
+        <br>
+        <p class="brand">Oppo Reno 8T</p>
+        <p class="harga">4.799.000</p>
+        <img class="img-fluid" src="/image/oppo reno 8t.svg" alt="">
+        <br><br>
+        <button class="button px-2 py-1">Beli Sekarang</button>
+        <br><br>
+        <p class="spek">8+256 GB</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="main-hero row mt-5 px-4">
+    <div class="card-wrapper col-2 mt-2">
+      <div class="card-1 text-center">
+        <br>
+        <p class="brand">Redmi 12</p>
+        <p class="harga">1.999.000</p>
+        <img class="img-fluid" src="/image/redmi 12.svg" alt="">
+        <br><br>
+        <button class="button px-2 py-1">Beli Sekarang</button>
+        <br><br>
+        <p class="spek">8+128 GB</p>
+      </div>
+    </div>
+
+    <div class="card-wrapper col-2 mt-2">
+      <div class="card-1 text-center">
+        <br>
+        <p class="brand">Redmi Note 12 Pro</p>
+        <p class="harga">3.699.000</p>
+        <img class="img-fluid" src="/image/redmi note 12 pro.svg" alt="">
+        <br><br>
+        <button class="button px-2 py-1">Beli Sekarang</button>
+        <br><br>
+        <p class="spek">8+256 GB</p>
+      </div>
+    </div>
+
+    <div class="card-wrapper col-2 mt-2">
+      <div class="card-1 text-center">
+        <br>
+        <p class="brand">Poco X5 5g</p>
+        <p class="harga">3.300.000</p>
+        <img class="img-fluid" src="/image/Poco x5.svg" alt="">
+        <br><br>
+        <button class="button px-2 py-1">Beli Sekarang</button>
+        <br><br>
+        <p class="spek">6+128 GB</p>
+      </div>
+    </div>
+
+    <div class="card-wrapper col-2 mt-2">
+      <div class="card-1 text-center">
+        <br>
+        <p class="brand">Vivo T1 Pro 5g</p>
+        <p class="harga">4.999.000</p>
+        <img class="img-fluid" src="/image/Vivo T1 Pro 5G.svg" alt="">
+        <br><br>
+        <button class="button px-2 py-1">Beli Sekarang</button>
+        <br><br>
+        <p class="spek">8+128 GB</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="main-hero row mt-5 px-4">
+    <div class="card-wrapper col-2 mt-2">
+      <div class="card-1 text-center">
+        <br>
+        <p class="brand">Redmi 12</p>
+        <p class="harga">1.999.000</p>
+        <img class="img-fluid" src="/image/redmi 12.svg" alt="">
+        <br><br>
+        <button class="button px-2 py-1">Beli Sekarang</button>
+        <br><br>
+        <p class="spek">8+128 GB</p>
+      </div>
+    </div>
+
+    <div class="card-wrapper col-2 mt-2">
+      <div class="card-1 text-center">
+        <br>
+        <p class="brand">Redmi Note 12 Pro</p>
+        <p class="harga">3.699.000</p>
+        <img class="img-fluid" src="/image/redmi note 12 pro.svg" alt="">
+        <br><br>
+        <button class="button px-2 py-1">Beli Sekarang</button>
+        <br><br>
+        <p class="spek">8+256 GB</p>
+      </div>
+    </div>
+
+    <div class="card-wrapper col-2 mt-2">
+      <div class="card-1 text-center">
+        <br>
+        <p class="brand">Poco X5 5g</p>
+        <p class="harga">3.300.000</p>
+        <img class="img-fluid" src="/image/Poco x5.svg" alt="">
+        <br><br>
+        <button class="button px-2 py-1">Beli Sekarang</button>
+        <br><br>
+        <p class="spek">6+128 GB</p>
+      </div>
+    </div>
+
+    <div class="card-wrapper col-2 mt-2">
+      <div class="card-1 text-center">
+        <br>
+        <p class="brand">Vivo T1 Pro 5g</p>
+        <p class="harga">4.999.000</p>
+        <img class="img-fluid" src="/image/Vivo T1 Pro 5G.svg" alt="">
+        <br><br>
+        <button class="button px-2 py-1">Beli Sekarang</button>
+        <br><br>
+        <p class="spek">8+128 GB</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="main-hero row mt-5 px-4">
+    <div class="card-wrapper col-2 mt-2">
+      <div class="card-1 text-center">
+        <br>
+        <p class="brand">Redmi Note 12</p>
+        <p class="harga">2.300.000</p>
+        <img class="img-fluid" src="/image/redmi note 12.svg" alt="">
+        <br><br>
+        <button class="button px-2 py-1">Beli Sekarang</button>
+        <br><br>
+        <p class="spek">6+128 GB</p>
+      </div>
+    </div>
+
+    <div class="card-wrapper col-2 mt-2">
+      <div class="card-1 text-center">
+        <br>
+        <p class="brand">Vivo V21</p>
+        <p class="harga">3.800.000</p>
+        <img class="img-fluid" src="/image/vivo v21.svg" alt="">
+        <br><br>
+        <button class="button px-2 py-1">Beli Sekarang</button>
+        <br><br>
+        <p class="spek">8+128 GB</p>
+      </div>
+    </div>
+
+    <div class="card-wrapper col-2 mt-2">
+      <div class="card-1 text-center">
+        <br>
+        <p class="brand">Samsung s23</p>
+        <p class="harga">8.999.000</p>
+        <img class="img-fluid" src="/image/samsung s23.svg" alt="">
+        <br><br>
+        <button class="button px-2 py-1">Beli Sekarang</button>
+        <br><br>
+        <p class="spek">8+128 GB</p>
+      </div>
+    </div>
+
+    <div class="card-wrapper col-2 mt-2">
+      <div class="card-1 text-center">
+        <br>
+        <p class="brand">Oppo Reno 8T</p>
+        <p class="harga">4.799.000</p>
+        <img class="img-fluid" src="/image/oppo reno 8t.svg" alt="">
+        <br><br>
+        <button class="button px-2 py-1">Beli Sekarang</button>
+        <br><br>
+        <p class="spek">8+256 GB</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="main-hero row mt-5 px-4">
+    <div class="card-wrapper col-2 mt-2">
+      <div class="card-1 text-center">
+        <br>
+        <p class="brand">Redmi Note 12</p>
+        <p class="harga">2.300.000</p>
+        <img class="img-fluid" src="/image/redmi note 12.svg" alt="">
+        <br><br>
+        <button class="button px-2 py-1">Beli Sekarang</button>
+        <br><br>
+        <p class="spek">6+128 GB</p>
+      </div>
+    </div>
+
+    <div class="card-wrapper col-2 mt-2">
+      <div class="card-1 text-center">
+        <br>
+        <p class="brand">Vivo V21</p>
+        <p class="harga">3.800.000</p>
+        <img class="img-fluid" src="/image/vivo v21.svg" alt="">
+        <br><br>
+        <button class="button px-2 py-1">Beli Sekarang</button>
+        <br><br>
+        <p class="spek">8+128 GB</p>
+      </div>
+    </div>
+
+    <div class="card-wrapper col-2 mt-2">
+      <div class="card-1 text-center">
+        <br>
+        <p class="brand">Samsung s23</p>
+        <p class="harga">8.999.000</p>
+        <img class="img-fluid" src="/image/samsung s23.svg" alt="">
+        <br><br>
+        <button class="button px-2 py-1">Beli Sekarang</button>
+        <br><br>
+        <p class="spek">8+128 GB</p>
+      </div>
+    </div>
+
+    <div class="card-wrapper col-2 mt-2">
+      <div class="card-1 text-center">
+        <br>
+        <p class="brand">Oppo Reno 8T</p>
+        <p class="harga">4.799.000</p>
+        <img class="img-fluid" src="/image/oppo reno 8t.svg" alt="">
+        <br><br>
+        <button class="button px-2 py-1">Beli Sekarang</button>
+        <br><br>
+        <p class="spek">8+256 GB</p>
+      </div>
+    </div>
+  </div>
+
+  </div>
+  </div>
+  <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+</section>
+<footer>
+  <div class="container">
+    <div class="footer row px-4">
+      <div class="col-3">
+        <br><br><br>
+        <ul>
+          <li>PRODUK</li>
+          <br>
+          <li>Redmi Note 12</li>
+          <li>Poco F4</li>
+          <li>Samsung A05s</li>
+          <li>Vivo V27e</li>
+          <li>Redmi 12</li>
+          <li>Xiaomi 12T</li>
+          <li>Samsung A05</li>
+          <li>Oppo A3s</li>
+          <br><br><br><br><br>
+        </ul>
+      </div>
+      <div class="col-3">
+        <br><br><br>
+        <ul>
+          <li>DUKUNGAN</li>
+          <br>
+          <li>Panduan Pengguna</li>
+          <li>Garansi</li>
+          <li>Gerai Terdekat</li>
+          <li>Service</li>
+        </ul>
+      </div>
+      <div class="col-3">
+        <br><br><br>
+        <ul>
+          <li>TENTANG</li>
+          <br>
+          <li>Tentang Kami</li>
+          <li>Kebijakan privasi</li>
+        </ul>
+      </div>
+      <div class="col-3 ">
+        <br><br><br>
+        <ul>
+          <li>Social Media</li>
+          <br>
+        </ul>
+        <div class="d-flex gap-3 px-2">
+          <img src="/image/Twitter.svg" alt="">
+          <img src="/image/Youtube.svg" alt="">
+          <img src="/image/Facebook.svg" alt="">
+          <img src="/image/Instagram.svg" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+  <hr>
+  <div class="copyrightText d-flex justify-content-between px-5 py-5">
+    <a>Copyright © 2022 - 2023 Gadget Store</a>
+    <a>Indonesia / Indonesia</a>
+  </div>
+</footer>
