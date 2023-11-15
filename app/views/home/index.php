de2517d1f739d4770d361d95dc6c10fd87bb972b
CancriPiscium <nyenkadendwika@gmail.com>
2023-11-15 10:22:11 +0800

    a

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -11,18 +11,17 @@
 </div>
 
 <?php foreach ($data['barang'] as $barang) : ?>
-
     <div class="container mt-5">
-
-        <div class="card" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title"><?= $barang['namaBarang']; ?></h5>
-                <h6 class="card-subtitle mb-2 text-muted"><?= $barang['merek']; ?></h6>
-                <p class="card-text"><?= $barang['stock']; ?></p>
-                <img class="card-text" src="<?= BASEURL ?>/img/gambar/<?= $barang['img']; ?>" alt="">
-                <a href="<?= BASEURL; ?>/barang" class="card-link">Kembali</a>
+        <div class="row">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title"><?= $barang['namaBarang']; ?></h5>
+                    <h6 class="card-subtitle mb-2 text-muted"><?= $barang['merek']; ?></h6>
+                    <p class="card-text"><?= $barang['stock']; ?></p>
+                    <img class="card-text" src="<?= BASEURL ?>/img/gambar/<?= $barang['img']; ?>" alt="">
+                    <a href="<?= BASEURL; ?>/barang" class="card-link">Kembali</a>
+                </div>
             </div>
         </div>
-
     </div>
 <?php endforeach; ?>
