a3d1fa5fcfd6318bd873c321cfc7151d9f40fe70
Dwipaaa <147568071+Dwipaaa@users.noreply.github.com>
2023-11-15 09:23:19 +0800

    Update index.php

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -4,28 +4,25 @@
         <h1 class="display-4">Selamat datang di website saya</h1>
         <hr class="my-4">
         <a class="btn btn-primary btn-l4" href="">klik
-            
+
         </a>
     </div>
 
 </div>
 
+<?php foreach ($data['barang'] as $barang) : ?>
 
-            <?php foreach ($data['barang'] as $barang) : ?>
+    <div class="container mt-5">
 
-                
-                <div class="container mt-5">
-                
-                    <div class="card" style="width: 18rem;">
-                        <div class="card-body">
-                            <h5 class="card-title"><?= $barang['namaBarang'];?></h5>
-                            <h6 class="card-subtitle mb-2 text-muted"><?= $barang['merek'];?></h6>
-                            <p class="card-text"><?= $barang['stock'];?></p>
-                            <img class="card-text" src="<?= BASEURL ?>/img/gambar/<?= $barang['img'];?>" alt="">    
-                            <a href="<?= BASEURL;?>/barang" class="card-link">Kembali</a>
-                        </div>
-                    </div>
-                
-                </div>
-            <?php endforeach; ?>
-        
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title"><?= $barang['namaBarang']; ?></h5>
+                <h6 class="card-subtitle mb-2 text-muted"><?= $barang['merek']; ?></h6>
+                <p class="card-text"><?= $barang['stock']; ?></p>
+                <img class="card-text" src="<?= BASEURL ?>/img/gambar/<?= $barang['img']; ?>" alt="">
+                <a href="<?= BASEURL; ?>/barang" class="card-link">Kembali</a>
+            </div>
+        </div>
+
+    </div>
+<?php endforeach; ?>
