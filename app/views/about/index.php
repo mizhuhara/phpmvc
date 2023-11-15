174df60482e5501cd65216451696f4e89dfdb86c
mizhuhara <110947967+mizhuhara@users.noreply.github.com>
2023-11-15 07:11:12 +0700

    Update index.php

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -4,3 +4,4 @@
     <img src="<?= BASEURL; ?>/img/poto1.jpg" alt="Kazuya" width="200" class="rounded-circle">
     <p>Halo, nama saya <?= $data['nama']; ?>, saya adalah <?=$data['pekerjaan'];?>, umur saya <?=$data['umur'];?></p>
 </div>
+<p></p>
