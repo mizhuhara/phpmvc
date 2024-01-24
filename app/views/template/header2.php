6902914c2f855a42d14b38fb1fe2cd4442600779
CancriPiscium <nyenkadendwika@gmail.com>
2024-01-24 08:51:19 +0800

    a

--- app/views/template/header2.php
+++ app/views/template/header2.php
@@ -29,7 +29,7 @@
         <div class="navbar-nav">
           <a class="nav-link active" aria-current="page" href="<?= BASEURL; ?>">Home</a>
           <a class="nav-link" href="<?= BASEURL; ?>/Product">Produk</a>
-          <a class="nav-link" aria-current="page" href="<?= BASEURL; ?>/login/logout">Log Out</a>
+          <a class="nav-link" aria-current="page" href="<?= BASEURL; ?>/login/logout">Login</a>
         </div>
       </div>
     </div>
