084d1a1cd047eb1735df788bd491371fa26629bf
CancriPiscium <nyenkadendwika@gmail.com>
2024-01-24 08:13:30 +0800

    2

--- app/views/barang/index.php
+++ app/views/barang/index.php
@@ -59,7 +59,7 @@
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <span class="mr-2 d-none d-lg-inline t small">Kazuya</span>
-                                <img class="img-profile rounded-circle" src="./img/profile.svg">
+                                <img class="img-profile rounded-circle" src="./img/PP2.jpg">
                             </a>
                         </li>
                     </ul>
